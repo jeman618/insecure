63ad0ec8552c0933c265fa8c6f746dc6c11187f7
Lucas Pierce <jackalnom@gmail.com>
2023-11-05 18:53:44 -0800

    unquoting from url lib

--- src/api/hello.py
+++ src/api/hello.py
@@ -2,6 +2,9 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 import sqlalchemy
 from src import database as db
+from urllib.parse import unquote
+
+
 
 router = APIRouter(
     prefix="/hello",
@@ -12,9 +15,9 @@ router = APIRouter(
 def say_hi(name: str):
     """ """
     print(name)
-        
+
     with db.engine.begin() as connection:
-        sql = f"SELECT greeting FROM characters WHERE character_name = '{name}'"
+        sql = f"SELECT greeting FROM characters WHERE character_name = '{unquote(name)}'"
         print(f"sql executed: {sql}")
         results = connection.execute(sqlalchemy.text(sql))
         for row in results:
